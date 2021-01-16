12db2eaea148c9551b0bedb2de8fabaca03495b1
DESKTOP-FTDQ7IC\PO <zeryoume@gmail.com>
2021-01-16 14:44:53 +0700

    string

---
+++ Workshop2/string/f_string.py
@@ -0,0 +1,6 @@
+name = "jim"
+age = 26
+
+result = f" My name is {name}, and I am {age}"
+
+print("result", result)  # output : "My name is jim, and I am 26"
---
+++ Workshop2/string/modify_string.py
@@ -0,0 +1,8 @@
+string = " Hello, World"
+
+print(string.upper())  # Output : "HELLO WORLD!"
+print(string.lower())  # Output : "hello world"
+print(string.strip())  # Output : "Hello, World"
+print(string.replace("H", "3"))  # Output : "jello, World"
+print(string.split(","))  # output : [' Hello', 'World!']
+print(len(string))  # 15
